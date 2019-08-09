6a33c7f74fa56235e5cabaffd8ccd82744e37dad
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-08-09 10:03:45 -0700

    updates

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,53 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function cardFunction(){
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response =>{
+    const artData = response.data.articles;
+    const artValue = 
+    Object.values(artData);
+
+    artValue.forEach(item =>{
+        item.forEach(items => { articleCard(items);
+
+        })
+
+})
+
+})
+
+}
+
+const view = document.querySelector('.cards-container');
+
+function articleCard(object){
+
+    console.log(object);
+
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+
+    const headlineDiv = document.createElement('div');
+    authorDiv.classList.add('author');
+
+    const imgDiv = document.createElement('div');
+
+    imgDiv.classList.add('img-container');
+
+    const imgTag = document.createElement('span');
+imgTag.src = object.authorPhoto;
+
+const spanTag = document.createElement('span');
+spanTag.textContent = object.authorName;
+
+cardDiv.appendChild(headlineDiv);
+cardDiv.appendChild(authorDiv);
+authorDiv.appendChild(imgDiv);
+authorDiv.appendChild(spanTag);
+imgDiv.appendChild(imgTag);
+
+view.appendChild(cardDiv);
+}
