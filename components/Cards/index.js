2f25d256c9aa8af36c70230a1a1ee33067bc08da
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-08-09 10:05:43 -0700

    why are you reading this im saucy

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function cardFunction(){
+
+    axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response =>{
+        const artData = response.data.articles;
+        const artValue = 
+        Object.values(artData);
+    
+        artValue.forEach(item =>{
+            item.forEach(items => { articleCard(items);
+    
+            })
+    
+    })
+    
+    })
+    
+    }
+    
+    const view = document.querySelector('.cards-container');
+    
+    function articleCard(object){
+    
+        console.log(object);
+    
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+    
+        const headlineDiv = document.createElement('div');
+        authorDiv.classList.add('author');
+    
+        const imgDiv = document.createElement('div');
+    
+        imgDiv.classList.add('img-container');
+    
+        const imgTag = document.createElement('span');
+    imgTag.src = object.authorPhoto;
+    
+    const spanTag = document.createElement('span');
+    spanTag.textContent = object.authorName;
+    
+    cardDiv.appendChild(headlineDiv);
+    cardDiv.appendChild(authorDiv);
+    authorDiv.appendChild(imgDiv);
+    authorDiv.appendChild(spanTag);
+    imgDiv.appendChild(imgTag);
+    
+    view.appendChild(cardDiv);
+    }
+    
